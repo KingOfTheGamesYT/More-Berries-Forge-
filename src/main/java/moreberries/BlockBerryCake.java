e1f335db0972a4da2eebe5f26d1977d9342c80cf
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2022-08-03 21:50:59 -0400

    Ported over the Berry Cake block class
    to Forge 1.16.5

--- src/main/java/moreberries/BlockBerryCake.java
+++ src/main/java/moreberries/BlockBerryCake.java
@@ -1,41 +1,37 @@
 package moreberries;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CakeBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.stat.Stats;
-import net.minecraft.util.ActionResult;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.WorldAccess;
-import net.minecraft.world.event.GameEvent;
+import net.minecraft.world.IWorld;
 
-import java.util.HashMap;
 
 public class BlockBerryCake extends CakeBlock {
 
 
-	public BlockBerryCake(Block.Settings settings) {
-		super(settings);
+	public BlockBerryCake(AbstractBlock.Properties properties) {
+		super(properties);
 		this.setDefaultState(this.getDefaultState().with(BITES, 0));
 	}
 
-	public static ActionResult tryEat(WorldAccess worldAccess, BlockPos blockPos, BlockState blockState, PlayerEntity playerEntity) {
-		if (!playerEntity.canConsume(false)) {
-			return ActionResult.PASS;
+	private ActionResultType eatSlice(IWorld world, BlockPos pos, BlockState state, PlayerEntity player) {
+		if (!player.canEat(false)) {
+			return ActionResultType.PASS;
 		} else {
-			playerEntity.incrementStat(Stats.EAT_CAKE_SLICE);
-			playerEntity.getHungerManager().add(2, 0.1F);
-			int i = (Integer)blockState.get(BITES);
-			worldAccess.emitGameEvent(playerEntity, GameEvent.EAT, blockPos);
+			player.addStat(Stats.EAT_CAKE_SLICE);
+			player.getFoodStats().addStats(2, 0.1F);
+			int i = (Integer)state.get(BITES);
 			if (i < 6) {
-				worldAccess.setBlockState(blockPos, (BlockState)blockState.with(BITES, i + 1), 3);
+				world.setBlockState(pos, (BlockState)state.with(BITES, i + 1), 3);
 			} else {
-				worldAccess.removeBlock(blockPos, false);
-				worldAccess.emitGameEvent(playerEntity, GameEvent.BLOCK_DESTROY, blockPos);
+				world.removeBlock(pos, false);
 			}
 
-			return ActionResult.SUCCESS;
+			return ActionResultType.SUCCESS;
 		}
 	}
 
